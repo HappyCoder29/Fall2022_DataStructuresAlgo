04e20d69638c3e3786c13e72dc88e67739e6375e
Ashish <ashish@northeastern.edu>
2022-11-13 10:53:29 -0800

    Checking in question 2

---
+++ MidTerm/Question2/Question2/src/Node.java
@@ -0,0 +1,13 @@
+
+
+public class Node <T> {
+    public T data;
+    public Node<T> left;
+    public Node<T> right;
+
+    public Node<T> parent;
+
+    public Node(T data){
+        this.data = data;
+    }
+}
