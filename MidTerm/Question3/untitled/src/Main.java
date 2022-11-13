b7113ad347ac67c5bf77ba0fb40ff665d1464737
Ashish <ashish@northeastern.edu>
2022-11-13 10:56:25 -0800

    Checking in question 3

---
+++ MidTerm/Question3/untitled/src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    // Given range between 5 and 8 it will print 6, 7, 8
+    private static Node<Integer> getBST(){
+        Node<Integer> root = new Node<>(8);
+        root.parent = root;
+
+        root.left = new Node<>(3);
+        root.right = new Node<>(10);
+        root.left.parent = root;
+        root.right.parent = root;
+
+
+        root.left.left = new Node<>(1);
+        root.left.right = new Node<>(6);
+        root.left.left.parent = root.left;
+        root.left.right.parent = root.left;
+
+        root.right.right = new Node<>(14);
+        root.right.right.parent = root.right;
+
+        root.left.right.left = new Node<>(4);
+        root.left.right.right = new Node<>(17);
+        root.left.right.left.parent = root.left.right;
+        root.left.right.right.parent = root.left.right;
+
+
+        root.right.right.left = new Node<>(13);
+        root.right.right.left.parent = root.right.right;
+
+
+
+        return root;
+    }
+
+}
