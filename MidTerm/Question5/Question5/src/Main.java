d43d6bb8c46f19871d944bde24e0c3aab9f5e279
Ashish <ashish@northeastern.edu>
2022-11-13 11:03:06 -0800

    Checking in question 5

---
+++ MidTerm/Question5/Question5/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    // arr = [0,0,0,0,0,1,1,1,1,2,2,5,5,5,8,9,10,11]
+    // values = [1,4,5,10]
+    // output = [5, -1, 12, 17]
+    private static int[] getStartOfEachValues(int[] arr, int[] values){
+        int[] indexes = new int[values.length];
+
+        return indexes;
+    }
+}
