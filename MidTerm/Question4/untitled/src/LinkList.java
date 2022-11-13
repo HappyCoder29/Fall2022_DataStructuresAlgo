ee9bfda24311c97391f4daebf79362617a3225f1
Ashish <ashish@northeastern.edu>
2022-11-13 10:59:17 -0800

    Checking in code for question 4

---
+++ MidTerm/Question4/untitled/src/LinkList.java
@@ -0,0 +1,14 @@
+public class LinkList  {
+
+    public Node head;
+
+    public int length = 0;
+
+    public LinkList() {
+        head = null;
+    }
+
+    public void insert(Integer data){
+        // Insert in sorted manner
+    }
+}
---
+++ MidTerm/Question4/untitled/src/Node.java
@@ -0,0 +1,10 @@
+public class Node  {
+    public Integer data;
+    public Node next;
+
+    public  Node(Integer data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
