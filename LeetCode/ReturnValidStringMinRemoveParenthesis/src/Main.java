f79101db9bdadc6ac00d9a070cae08fc6bd6804c
DrillMaps <ashish@drillmaps.com>
2022-12-08 19:16:25 -0800

    Checking in code for Return Valid string https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/

---
+++ LeetCode/ReturnValidStringMinRemoveParenthesis/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Stack;
+//https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(returnValidString("))(("));
+        System.out.println("Hello world!");
+    }
+
+    private static String returnValidString(String str){
+        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> indexes  = new ArrayList<>();
+        for(int i = 0 ; i < str.length(); i ++) {
+            if(str.charAt(i) == '('){
+                stack.push(i);
+            }else if (str.charAt(i) == ')'){
+                if(stack.isEmpty()){
+                    indexes.add(i);
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        while(!stack.isEmpty()){
+            indexes.add(stack.pop());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < str.length(); i ++){
+            if(!indexes.contains(i)){
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
